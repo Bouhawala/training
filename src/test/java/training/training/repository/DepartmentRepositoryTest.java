d649d6420693a09b2b6890f5258bfff11d8819b2
Mohamed Bouhawala <mohamedbouhawala@Mohameds-MBP.fritz.box>
2023-02-15 23:04:26 +0100

    add repository test cases

--- src/test/java/training/training/repository/DepartmentRepositoryTest.java
+++ src/test/java/training/training/repository/DepartmentRepositoryTest.java
@@ -1,6 +1,9 @@
 package training.training.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,5 +42,62 @@ public class DepartmentRepositoryTest {
 
         assertEquals(department.getDepartmentName(), "IT");
     }
-    
-}       
+
+    @Test
+    public void findAll_success() {
+        List<Department> allDepartments = departmentRepository.findAll();
+        assertThat(allDepartments.size()).isGreaterThanOrEqualTo(1);
+    }
+
+    @Test
+    public void should_save_department() {
+      Department department = departmentRepository.save(Department.builder()
+            .departmentName("IT")
+            .departmentAddress("Schifflange")
+            .departmentCode("IT-06")
+            .departmentId(1L)
+            .build());
+  
+      assertThat(department).hasFieldOrPropertyWithValue("departmentName", "IT");
+      assertThat(department).hasFieldOrPropertyWithValue("departmentAddress", "Schifflange");
+      assertThat(department).hasFieldOrPropertyWithValue("departmentCode", "IT-06");
+    }
+
+    @Test
+    public void should_update_department_by_id() {
+      Department department1 = Department.builder()
+        .departmentName("IT")
+        .departmentAddress("Schifflange")
+        .departmentCode("IT-06")
+        .build();
+      entityManager.persist(department1);
+  
+      Department department2 = Department.builder()
+        .departmentName("EEE")
+        .departmentAddress("Esch/Alzette")
+        .departmentCode("EEE-011")
+        .departmentId(1L)
+        .build();
+  
+      Department updatedDepartment = Department.builder()
+        .departmentName("Updated-EEE")
+        .departmentAddress("Updated-Esch/Alzette")
+        .departmentCode("Updated-EEE-011")
+        .departmentId(1L)
+        .build();
+
+      Department dep = departmentRepository.findById(department2.getDepartmentId()).get();
+      dep.setDepartmentName(updatedDepartment.getDepartmentName());
+      dep.setDepartmentAddress(updatedDepartment.getDepartmentAddress());
+      dep.setDepartmentCode(updatedDepartment.getDepartmentCode());
+      departmentRepository.save(dep);
+  
+      Department checkDep = departmentRepository.findById(department2.getDepartmentId()).get();
+      
+      assertThat(checkDep.getDepartmentId()).isEqualTo(department2.getDepartmentId());
+      assertThat(checkDep.getDepartmentName()).isEqualTo(updatedDepartment.getDepartmentName());
+      assertThat(checkDep.getDepartmentAddress()).isEqualTo(updatedDepartment.getDepartmentAddress());
+      assertThat(checkDep.getDepartmentCode()).isEqualTo(updatedDepartment.getDepartmentCode());
+    }
+
+}
